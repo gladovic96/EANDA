2a20688c50505969515bf5b36bc061a0806f6a41
gladovic96 <137275778+gladovic96@users.noreply.github.com>
2023-06-23 15:48:15 +0200

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import styles from './Navbar.module.css';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className={styles.nav}>
+      <div className={styles.container}>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <h1>
+            EANDA
+            <br />
+            <span>Steam Machines</span>
+          </h1>
+          <li>Our Product</li>
+          <li>Contact Us</li>
+        </ul>
+        <div onClick={handleNav}>
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
