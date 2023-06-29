85f4463e679ef0c330f51b3feebfaf89fd4309c0
gladovic96 <137275778+gladovic96@users.noreply.github.com>
2023-06-29 19:23:14 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Product from './components/Product';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
+      <About />
+      <Product />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
